2762ca7c4fabaa9a24daf280e177ba0078515e60
Toan Dinh <taplamtiktok686@gmail.com>
2025-07-04 14:35:02 +0700

    cách sử dụng Router trong ExpressJS

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 var app = express();
+var router1 = express.Router();
+var router2 = express.Router();
+
+
+
 
 app.get('/', (req, res) => {
   res.json('Home')
 })
 
 app.listen(3000, () => {
-  console.log(`Server startedon port`)
-});
+  console.log(`Server started on port`);
+}
