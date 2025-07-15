b98aa3e52719b45f331914c3942d9481c0593592
minhvusotatek <minh.vu5@sotatek.com>
2025-07-15 14:18:27 +0700

    Cap nhat link khoa hoc trong ReadMe

--- main.js
+++ main.js
@@ -1,13 +1,8 @@
-$.ajax({
-    url: '/user?page=3',
-    type: 'GET'
-}).then(data=>{
-    console.log(data);
-    for(let i = 0; i< data.length;i++) {
-        const element = data[i];
-
-        var item$(`
-            <h1>${element.username} : ${element.password}
-            `)
+$('#demo').pagination({
+    dataSource: [1, 2, 3, 4, 5, 6, 7, ... , 195],
+    callback: function(data, pagination) {
+        // template method of yourself
+        var html = template(data);
+        dataContainer.html(html);
     }
-}
+})
